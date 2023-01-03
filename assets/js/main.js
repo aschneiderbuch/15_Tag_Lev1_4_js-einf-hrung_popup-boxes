cfcf5192973435e9485792a00e7b60bcf804aa57
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 15:02:29 +0100

    alert

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+window.alert("Hallo Welt")
+
+window.prompt("Bitte geben Se Ihren Namen ein.")
+
+window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel")  // Cancel steht jetzt im prompt InputFeld
