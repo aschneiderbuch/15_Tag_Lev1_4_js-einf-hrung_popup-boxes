3728e7df6246817ba294c417fb6869633091a581
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 15:12:53 +0100

    alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@ window.alert("Hallo Welt")
 window.prompt("Bitte geben Se Ihren Namen ein.")
 
 window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel")  // Cancel steht jetzt im prompt InputFeld
+
+window.confirm("Stimmen Sie meiner Meinung zu?")
+
+
+let alter = prompt ("Gib mr bitte dein Alter: ")
+console.log(alter)      // gibt diese prompt Antwort zurück
+
+let b = 5
+console.log(b) // 5
+let a = b * 5 -3 // 22
+console.log(a)
